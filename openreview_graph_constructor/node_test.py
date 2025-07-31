5ea92df0d4b5413ef54fa3f22090f3c8c79ccecc
xujingjun <12210357@mail.sustech.edu.cn>
2025-07-31 22:03:27 +0800

    clarify the node table and edge table, and build the python interface

---
+++ openreview_graph_constructor/node_test.py
@@ -0,0 +1,172 @@
+from sqlDatabaseConstructor import sqlDatabaseConstructor
+
+sql_database = sqlDatabaseConstructor()
+
+############ get single node features ############
+# papers table
+'''
+node = sql_database.get_node_features("papers", {"paper_openreview_id": "00SnKBGTsz"})
+print(node)
+'''
+
+# reviews table
+'''
+node = sql_database.get_node_features("reviews", {"review_openreview_id": "Is5Qh2Gs5x"})
+print(node)
+'''
+
+# authors table
+'''
+node = sql_database.get_node_features("authors", {"author_openreview_id": "~A-Long_Jin1"})
+print(node)
+'''
+
+# revisions table
+'''
+node = sql_database.get_node_features("revisions", {"modified_openreview_id": "AhzvtTGKFO"})
+print(node)
+'''
+
+############ get all nodes without features from tables ############
+# papers table
+'''
+df = sql_database.get_all_nodes("papers")
+print(len(df))
+print(df.iloc[0])
+'''
+
+# reviews table
+'''
+df = sql_database.get_all_nodes("reviews")
+print(len(df))
+print(df.iloc[0])
+'''
+
+# authors table
+'''
+df = sql_database.get_all_nodes("authors")
+print(len(df))
+print(df.iloc[0])
+'''
+
+# revisions table
+'''
+df = sql_database.get_all_nodes("revisions")
+print(len(df))
+print(df.iloc[0])
+'''
+
+############ get all nodes with features from tables ############
+# papers table
+'''
+df = sql_database.get_all_node_features("papers")
+print(len(df))
+print(df.iloc[0])
+'''
+
+# reviews table
+'''
+df = sql_database.get_all_node_features("reviews")
+print(len(df))
+print(df.iloc[0])
+'''
+
+# authors table
+'''
+df = sql_database.get_all_node_features("authors")
+print(len(df))
+print(df.iloc[0])
+'''
+
+# revisions table
+'''
+df = sql_database.get_all_node_features("revisions")
+print(len(df))
+print(df.iloc[0])
+'''
+
+############ delete a single node and insert it back ############
+# papers table
+'''
+original_node_features = sql_database.delete_node("papers", {"paper_openreview_id": "zGej22CBnS"})
+node = sql_database.get_node_features("papers", {"paper_openreview_id": "zGej22CBnS"})
+print(node)
+
+sql_database.insert_node("papers", node_features=original_node_features)
+node = sql_database.get_node_features("papers", {"paper_openreview_id": "zGej22CBnS"})
+print(node)
+'''
+
+# reviews table
+'''
+original_node_features = sql_database.delete_node("reviews", {"review_openreview_id": "DHwZxFryth"})
+node = sql_database.get_node_features("reviews", {"review_openreview_id": "DHwZxFryth"})
+print(node)
+
+sql_database.insert_node("reviews", node_features=original_node_features)
+node = sql_database.get_node_features("reviews", {"review_openreview_id": "DHwZxFryth"})
+print(node)
+'''
+
+# revisions table
+'''
+original_node_features = sql_database.delete_node("revisions", {"modified_openreview_id": "yfHQOp5zWc"})
+node = sql_database.get_node_features("revisions", {"modified_openreview_id": "yfHQOp5zWc"})
+print(node)
+
+sql_database.insert_node("revisions", node_features=original_node_features)
+node = sql_database.get_node_features("revisions", {"modified_openreview_id": "yfHQOp5zWc"})
+print(node)
+'''
+
+# authors table
+'''
+original_node_features = sql_database.delete_node("authors", {"author_openreview_id": "~ishmam_zabir1"})
+node = sql_database.get_node_features("authors", {"author_openreview_id": "~ishmam_zabir1"})
+print(node)
+
+sql_database.insert_node("authors", node_features=original_node_features)
+node = sql_database.get_node_features("authors", {"author_openreview_id": "~ishmam_zabir1"})
+print(node)
+'''
+
+############ get the features from a node and update it back ############
+# papers tables
+'''
+node = sql_database.get_node_features("papers", {"paper_openreview_id": "zGej22CBnS"})
+print(node)
+
+sql_database.update_node("papers", node_features=node)
+node = sql_database.get_node_features("papers", {"paper_openreview_id": "zGej22CBnS"})
+print(node)
+'''
+
+# reviews table
+'''
+node = sql_database.get_node_features("reviews", {"review_openreview_id": "DHwZxFryth"})
+print(node)
+
+sql_database.update_node("reviews", node_features=node)
+node = sql_database.get_node_features("reviews", {"review_openreview_id": "DHwZxFryth"})
+print(node)
+'''
+
+# revisions table
+'''
+node = sql_database.get_node_features("revisions", {"modified_openreview_id": "yfHQOp5zWc"})
+print(node)
+
+sql_database.update_node("revisions", node_features=node)
+node = sql_database.get_node_features("revisions", {"modified_openreview_id": "yfHQOp5zWc"})
+print(node)
+'''
+
+# authors table
+'''
+node = sql_database.get_node_features("authors", {"author_openreview_id": "~ishmam_zabir1"})
+print(node)
+
+sql_database.update_node("authors", node_features=node)
+node = sql_database.get_node_features("authors", {"author_openreview_id": "~ishmam_zabir1"})
+print(node)
+'''
