4189cc742a719dc492ac270b9a835c933ff0d775
Chongshan0Lin <chongshan0lin@gmail.com>
2025-08-01 10:54:47 +0800

    Subfigure extraction from latex source code passes testing. Next step: recursive extraction from json file and paragraph level extraction

--- examples/crawler_job_specified.py
+++ examples/crawler_job_specified.py
@@ -13,7 +13,7 @@ cj = CrawlerJob(dest_dir="download")
 # print(arxiv_ids)
 # print(len(arxiv_ids))
 
-arxiv_ids = {'2507.10539v1'}
+arxiv_ids = {'2412.17767v2'}
 
 cj.initialize_paper_tasks(arxiv_ids=arxiv_ids)
 
@@ -22,14 +22,14 @@ print(f"arxiv ids: {arxiv_ids}")
 cj.download_papers(arxiv_ids=arxiv_ids)
 
 
-arxiv_ids = cj.select_unproceeded_task(task_type="paper_graph", max_results=400)
 # arxiv_ids = cj.select_proceeded_task(task_type="paper_graph", max_results=10)
+arxiv_ids = cj.select_unproceeded_task(task_type="paper_graph", max_results=400)
 print(f"arxiv ids: {arxiv_ids}")
 cj.process_paper_graphs(arxiv_ids=arxiv_ids)
 
 
-# arxiv_ids = cj.select_unproceeded_task(task_type="paragraph", max_results=1000)
 # arxiv_ids = cj.select_proceeded_task(task_type="paper_graph", max_results=10)
+# arxiv_ids = cj.select_unproceeded_task(task_type="paragraph", max_results=1000)
 # print(f"arxiv ids: {arxiv_ids}")
 # cj.process_paper_paragraphs(arxiv_ids=arxiv_ids)
 
