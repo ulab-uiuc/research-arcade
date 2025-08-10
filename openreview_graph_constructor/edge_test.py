087a28585e664e008c943974b61afa69e80fe33a
Jingjun XU <12210357@mail.sustech.edu.cn>
2025-08-10 10:03:52 +0000

    refine database

--- openreview_graph_constructor/edge_test.py
+++ openreview_graph_constructor/edge_test.py
@@ -7,19 +7,19 @@ sql_database = sqlDatabaseConstructor()
 ############ get single edge features ############
 # papers_authors table
 '''
-edge = sql_database.get_edge_features("papers_authors", {"paper_openreview_id": "00SnKBGTsz", "author_openreview_id": "~Elias_Stengel-Eskin1"})
+edge = sql_database.get_edge_features_by_id("papers_authors", {"paper_openreview_id": "00SnKBGTsz", "author_openreview_id": "~Elias_Stengel-Eskin1"})
 print(edge)
 '''
 
 # papers_reviews table
 '''
-edge = sql_database.get_edge_features("papers_reviews", {"paper_openreview_id": "00SnKBGTsz", "review_openreview_id": "13mj0Rtn5W"})
+edge = sql_database.get_edge_features_by_id("papers_reviews", {"paper_openreview_id": "00SnKBGTsz", "review_openreview_id": "13mj0Rtn5W"})
 print(edge)
 '''
 
 # papers_revisions table
 '''
-edge = sql_database.get_edge_features("papers_revisions", {"paper_openreview_id": "00SnKBGTsz", "revision_openreview_id": "2hQgqdDwZq"})
+edge = sql_database.get_edge_features_by_id("papers_revisions", {"paper_openreview_id": "00SnKBGTsz", "revision_openreview_id": "2hQgqdDwZq"})
 print(edge)
 '''
 
@@ -48,34 +48,34 @@ print(df.head())
 ############ delete a single edge and insert it back ############
 # papers_authors table
 '''
-original_edge_features = sql_database.delete_edge("papers_authors", {"paper_openreview_id": "zqzsZ5cXbB", "author_openreview_id": "~Zhi_Zhang4"})
-edge = sql_database.get_edge_features("papers_authors", {"paper_openreview_id": "zqzsZ5cXbB", "author_openreview_id": "~Zhi_Zhang4"})
+original_edge_features = sql_database.delete_edge_by_id("papers_authors", {"paper_openreview_id": "zqzsZ5cXbB", "author_openreview_id": "~Zhi_Zhang4"})
+edge = sql_database.get_edge_features_by_id("papers_authors", {"paper_openreview_id": "zqzsZ5cXbB", "author_openreview_id": "~Zhi_Zhang4"})
 print(edge)
 
 sql_database.insert_edge("papers_authors", edge_features=original_edge_features)
-edge = sql_database.get_edge_features("papers_authors", {"paper_openreview_id": "zqzsZ5cXbB", "author_openreview_id": "~Zhi_Zhang4"})
+edge = sql_database.get_edge_features_by_id("papers_authors", {"paper_openreview_id": "zqzsZ5cXbB", "author_openreview_id": "~Zhi_Zhang4"})
 print(edge)
 '''
 
 # papers_reviews table
 '''
-original_edge_features = sql_database.delete_edge("papers_reviews", {"paper_openreview_id": "zxbQLztmwb", "review_openreview_id": "9DZHHl3PgM"})
-edge = sql_database.get_edge_features("papers_reviews", {"paper_openreview_id": "zxbQLztmwb", "review_openreview_id": "9DZHHl3PgM"})
+original_edge_features = sql_database.delete_edge_by_id("papers_reviews", {"paper_openreview_id": "zxbQLztmwb", "review_openreview_id": "9DZHHl3PgM"})
+edge = sql_database.get_edge_features_by_id("papers_reviews", {"paper_openreview_id": "zxbQLztmwb", "review_openreview_id": "9DZHHl3PgM"})
 print(edge)
 
 sql_database.insert_edge("papers_reviews", edge_features=original_edge_features)
-edge = sql_database.get_edge_features("papers_reviews", {"paper_openreview_id": "zxbQLztmwb", "review_openreview_id": "9DZHHl3PgM"})
+edge = sql_database.get_edge_features_by_id("papers_reviews", {"paper_openreview_id": "zxbQLztmwb", "review_openreview_id": "9DZHHl3PgM"})
 print(edge)
 '''
 
 # papers_revisions table
 '''
-original_edge_features = sql_database.delete_edge("papers_revisions", {"paper_openreview_id": "zmmfsJpYcq", "revision_openreview_id": "GSKUBuUoMz"})
-edge = sql_database.get_edge_features("papers_revisions", {"paper_openreview_id": "zmmfsJpYcq", "revision_openreview_id": "GSKUBuUoMz"})
+original_edge_features = sql_database.delete_edge_by_id("papers_revisions", {"paper_openreview_id": "zmmfsJpYcq", "revision_openreview_id": "GSKUBuUoMz"})
+edge = sql_database.get_edge_features_by_id("papers_revisions", {"paper_openreview_id": "zmmfsJpYcq", "revision_openreview_id": "GSKUBuUoMz"})
 print(edge)
 
 sql_database.insert_edge("papers_revisions", edge_features=original_edge_features)
-edge = sql_database.get_edge_features("papers_revisions", {"paper_openreview_id": "zmmfsJpYcq", "revision_openreview_id": "GSKUBuUoMz"})
+edge = sql_database.get_edge_features_by_id("papers_revisions", {"paper_openreview_id": "zmmfsJpYcq", "revision_openreview_id": "GSKUBuUoMz"})
 print(edge)
 '''
 
@@ -83,13 +83,13 @@ print(edge)
 # papers_authors table
 # paper neighboring authors
 '''
-df = sql_database.get_neighborhood("papers_authors", {"paper_openreview_id": "00SnKBGTsz"})
+df = sql_database.get_neighborhood_by_id("papers_authors", {"paper_openreview_id": "00SnKBGTsz"})
 print(len(df))
 print(df.head())
 '''
 # author neighboring papers
 '''
-df = sql_database.get_neighborhood("papers_authors", {"author_openreview_id": "~Elias_Stengel-Eskin1"})
+df = sql_database.get_neighborhood_by_id("papers_authors", {"author_openreview_id": "~Elias_Stengel-Eskin1"})
 print(len(df))
 print(df.head())
 '''
@@ -97,13 +97,13 @@ print(df.head())
 # papers_reviews table
 # paper neighboring reviews
 '''
-df = sql_database.get_neighborhood("papers_reviews", {"paper_openreview_id": "00SnKBGTsz"})
+df = sql_database.get_neighborhood_by_id("papers_reviews", {"paper_openreview_id": "00SnKBGTsz"})
 print(len(df))
 print(df.head())
 '''
 # review neighboring papers
 '''
-df = sql_database.get_neighborhood("papers_reviews", {"review_openreview_id": "13mj0Rtn5W"})
+df = sql_database.get_neighborhood_by_id("papers_reviews", {"review_openreview_id": "13mj0Rtn5W"})
 print(len(df))
 print(df.head())
 '''
@@ -111,13 +111,13 @@ print(df.head())
 # papers_revisions table
 # paper neighboring revisions
 '''
-df = sql_database.get_neighborhood("papers_revisions", {"paper_openreview_id": "00SnKBGTsz"})
+df = sql_database.get_neighborhood_by_id("papers_revisions", {"paper_openreview_id": "00SnKBGTsz"})
 print(len(df))
 print(df.head())
 '''
 # revision neighboring papers
 '''
-df = sql_database.get_neighborhood("papers_revisions", {"revision_openreview_id": "2hQgqdDwZq"})
+df = sql_database.get_neighborhood_by_id("papers_revisions", {"revision_openreview_id": "2hQgqdDwZq"})
 print(len(df))
 print(df.head())
 '''
