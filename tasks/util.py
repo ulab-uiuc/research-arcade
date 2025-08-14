3cabfe1dea84c2587bf9f96f3ed6646a2746e0c8
Chongshan0Lin <chongshan0lin@gmail.com>
2025-08-15 01:00:20 +0800

    Create selection clause

--- tasks/util.py
+++ tasks/util.py
@@ -1,27 +1,64 @@
 import psycopg2
-def select_papers_with_criteria(min_paragraph = 1, min_figure = 0, min_citation = 0, min_table = 0):
+
+def select_papers_with_criteria(
+    min_paragraph=10, min_figure=0, min_citation=0, min_table=0
+):
     """
-    Select paper with at least one 
+    Return arxiv_ids from papers that meet all provided minimum counts.
+    Assumes child tables link to papers via columns named *_arxiv_id.
+    Adjust column/table names if yours differ.
     """
 
     conn = psycopg2.connect(
-            host="localhost",
-            port="5433",
-            dbname="postgres",
-            user="cl195"
-        )
-    conn.autocommit = True
-    cur = conn.cursor()
+        host="localhost", dbname="postgres", user="postgres",
+        password="Lcs20031121!", port="5432"
+    )
+    try:
+        conditions = []
+        params = []
 
-    cur.execute("""
-    SELECT p.*
-    FROM paper AS p
-    JOIN (
-    SELECT arxiv_id, COUNT(*) AS para_cnt
-    FROM paragraphs
-    GROUP BY arxiv_id
-    HAVING COUNT(*) >= %s
-    ) AS par ON par.arxiv_id = p.arxiv_id;
-    """, (min_paragraph,))
+        if min_paragraph > 0:
+            conditions.append("""
+                (SELECT COUNT(*)
+                FROM paragraphs pr
+                WHERE pr.paper_arxiv_id = p.arxiv_id) >= %s
+            """)
+            params.append(min_paragraph)
 
-    return cur.fetchall()
+        if min_figure > 0:
+            conditions.append("""
+                (SELECT COUNT(*)
+                FROM figures fg
+                WHERE fg.paper_arxiv_id = p.arxiv_id) >= %s
+            """)
+            params.append(min_figure)
+
+        if min_citation > 0:
+            conditions.append("""
+                (SELECT COUNT(*)
+                FROM citations ct
+                WHERE ct.citing_arxiv_id = p.arxiv_id) >= %s
+            """)
+            params.append(min_citation)
+
+        if min_table > 0:
+            conditions.append("""
+                (SELECT COUNT(*)
+                FROM tables tb
+                WHERE tb.paper_arxiv_id = p.arxiv_id) >= %s
+            """)
+            params.append(min_table)
+
+        where_clause = ("WHERE " + " AND ".join(conditions)) if conditions else ""
+
+        sql = f"""
+            SELECT p.arxiv_id
+              FROM papers AS p
+              {where_clause}
+        """
+
+        with conn, conn.cursor() as cur:
+            cur.execute(sql, tuple(params))
+            return [row[0] for row in cur.fetchall()]
+    finally:
+        conn.close()
