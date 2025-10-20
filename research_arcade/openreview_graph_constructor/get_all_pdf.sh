2a6e82686b93d4a649c0917f74624d3680321da4
Chongshan0Lin <chongshan0lin@gmail.com>
2025-10-20 00:48:38 -0500

    Reorganize directory structures

--- research_arcade/openreview_graph_constructor/get_all_pdf.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-
-for i in {1..30}; do
-    python get_all_pdf.py
-    tmux capture-pane -J -pS - > /Users/xujingjun/SUSTech/Research/LLM_copy/Graph_LLM/Code/formal-code/tmux_full_log.txt
-done
